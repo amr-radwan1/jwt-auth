3de3a84f66d6077e0fce90efdd1a14276040c68a
radwanam <radwanam@sheridancollege.ca>
2024-04-14 22:58:34 +0200

    first commit

---
+++ src/main/java/com/amr/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package com.amr.security.config;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+        // JWT token needs to be passed in a header
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // at 7 because "Bearer " is 7 characters
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+    }
+
+}
---
+++ src/main/java/com/amr/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.amr.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
